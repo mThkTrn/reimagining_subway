f4b8248a94ffdba5f2196dd30ae42848cfe2a3e3
Ilovenewyork <128846821+mThkTrn@users.noreply.github.com>
2024-04-09 22:45:21 -0400

    added more code

--- data_fetch.py
+++ data_fetch.py
@@ -9,7 +9,7 @@ url = "https://data.ny.gov/resource/wujg-7c2s.json"
 
 params = {
     "$where": "transit_timestamp >= '2023-03-01T00:00:00' AND transit_timestamp < '2023-03-02T00:00:00'",
-    "$limit": 500000 # set this arbitrarily high
+    "$limit": 5_000_000 # set this arbitrarily high
 }
 
 response = requests.get(url, params=params)
@@ -17,7 +17,7 @@ response = requests.get(url, params=params)
 
 if response.status_code == 200:
     data = pd.DataFrame(response.json())
-    data.to_csv("data/subway_data.csv")
+    data.to_csv("data/data.csv", index = False)
 else:
     print(f"Error: {response.status_code} - {response.text}")
 
