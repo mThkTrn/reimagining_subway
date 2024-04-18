855b51cb64a63050aae17d514a2dce816bf8e4f8
mThkTrn <madhavendra.thakur26@trinityschoolnyc.org>
2024-04-18 15:26:19 -0400

    added code

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 import flask
 from flask import Flask
 
@@ -9,20 +9,29 @@ def create_app(config=None):
     app.config.update(dict(DEBUG=True))
     app.config.update(config or {})
 
+
+    boroughs = set()
+    data = json.load(open("data/data.json"))
+    for station in data.keys():
+        boroughs.add(data[station]["borough"])
+    
     # Definition of the routes. Put them into their own file. See also
     # Flask Blueprints: http://flask.pocoo.org/docs/latest/blueprints
     @app.route("/")
     def home():
-        return  flask.render_template("index.html")
+        return  flask.render_template("index.html", boroughs = boroughs)
     
-    @app.route("/index.html")
+    @app.route("/index")
     def index():
-        return  flask.render_template("index.html")
+        return  flask.render_template("index.html", boroughs = boroughs)
 
-    @app.route("/about.html")
+    @app.route("/about")
     def about():
-        return flask.render_template('about.html')
-
+        return flask.render_template('about.html', boroughs = boroughs)
+    
+    @app.route("/borough_data/<borough>")
+    def subway_data(borough):
+        return flask.render_template('borough_data.html', borough = borough, boroughs = boroughs)
     return app
 
 
