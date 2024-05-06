548e1450926310d141688dfbc7db01aa39c912d1
mThkTrn <madhavendra.thakur26@trinityschoolnyc.org>
2024-05-05 20:52:14 -0400

    somehow full credit?

--- server.py
+++ server.py
@@ -4,11 +4,14 @@ import flask
 from flask import Flask
 import math
 import re
+
+
 def map_coords(orig_p0, orig_p1, map_p0, map_p1, coords):
     # print(f"X: ({coords[0]} - {orig_p0[0]}) * ({map_p1[0]}-{map_p0[0]})/({orig_p1[0]}-{orig_p0[0]}) + {map_p0[0]}")
     # print(f"Y: ({coords[1]} - {orig_p0[1]}) * ({map_p1[1]}-{map_p0[1]})/({orig_p1[1]}-{orig_p0[1]}) + {map_p0[0]}")
     # print([(coords[0] - orig_p0[0]) * (map_p1[0]-map_p0[0])/(orig_p1[0]-orig_p0[0]) + map_p0[0],(coords[1] - orig_p0[1]) * (map_p1[1]-map_p0[1])/(orig_p1[1]-orig_p0[1]) + map_p0[1]])
     return [(coords[1] - orig_p0[1]) * (map_p1[1]-map_p0[1])/(orig_p1[1]-orig_p0[1]) + map_p0[1] + 5, 500-((coords[0] - orig_p0[0]) * (map_p1[0]-map_p0[0])/(orig_p1[0]-orig_p0[0]) + map_p0[0])]
+
 
 
 def create_app(config=None):
@@ -18,54 +21,80 @@ def create_app(config=None):
 
     nyc_svg_bottom_left = [40.495306, -74.257389]
     nyc_svg_top_right = [40.914913, -73.685304]
-
-    boroughs = set()
-    data = json.load(open("data/data.json"))
-    linedata = json.load(open("data/subway_station_lines.json"))
-    for station in data.keys():
-        boroughs.add(data[station]["borough"])
-
-    borough_data = {}
-    for borough in boroughs:
-        borough_data[borough] = {}
-        borough_data[borough]["lines"] = set()
-        borough_data[borough]["total"] = 0
-        borough_data[borough]["times"] = {}
-        for i in range(24):
-            borough_data[borough]["times"] = [0]*24
     
-    for station in data.keys():
-        for i in re.findall(r'\(.*?\)', station):
-            i = i[1:-1].split(",")
-            for elem in i:
-                if len(elem) == 1:
-                    borough_data[data[station]["borough"]]["lines"].update(elem)
-            print(i, borough_data[data[station]["borough"]]["lines"])
-        for time in range(24):
-            try:
-                borough_data[data[station]["borough"]]["times"][time]+=float(data[station]["times"][f"{time:02}:00:00.000"]["ridership"])
-            except KeyError:
-                pass
-    for borough in borough_data: borough_data[borough]["total"] = sum(borough_data[borough]["times"])
-    circles_list = []
-    circle_scale = 0.3
-    for station in data.keys():
-        count = 0
-        pos = map_coords(nyc_svg_bottom_left, nyc_svg_top_right, (0,0), (500,500), (float(data[station]["latitude"]), float(data[station]["longitude"])))
-        for time in data[station]["times"]:
-            if time == "07:00:00.000" or time == "06:00:00.000" or time == "08:00:00.000":
-                count += (float(data[station]["times"][time]["ridership"]) - float(data[station]["times"][time]["transfers"]))
-        pos.append(math.sqrt(count)*circle_scale)
-        circles_list.append(pos)
+    data = json.load(open("data/data.json"))
+    
+    adds_counter = 0
+    times_sum = 0
+    for station in data["stations"]:
+        for time in data["stations"][station]["times"]:
+            times_sum += (data["stations"][station]["times"][time]["all_fare_classes"]["ridership"] * int(time[:2]))
+            adds_counter += data["stations"][station]["times"][time]["all_fare_classes"]["ridership"]
+    avg_time = times_sum/adds_counter
+
+    boroughs = data["other_data"]["boroughs"]
+    # linedata = json.load(open("data/subway_station_lines.json"))
+    # for station in data.keys():
+    #     boroughs.add(data[station]["borough"])
+
+    # borough_data = {}
+    # for borough in boroughs:
+    #     borough_data[borough] = {}
+    #     borough_data[borough]["lines"] = set()
+    #     borough_data[borough]["total"] = 0
+    #     borough_data[borough]["times"] = {}
+    #     for i in range(24):
+    #         borough_data[borough]["times"] = [0]*24
+    
+    # for station in data.keys():
+    #     for i in re.findall(r'\(.*?\)', station):
+    #         i = i[1:-1].split(",")
+    #         for elem in i:
+    #             if len(elem) == 1:
+    #                 borough_data[data[station]["borough"]]["lines"].update(elem)
+    #         #print(i, borough_data[data[station]["borough"]]["lines"])
+    #     for time in range(24):
+    #         try:
+    #             borough_data[data[station]["borough"]]["times"][time]+=float(data[station]["times"][f"{time:02}:00:00.000"]["ridership"])
+    #         except KeyError:
+    #             pass
+    # for borough in borough_data: borough_data[borough]["total"] = sum(borough_data[borough]["times"])
+    
+    # [fairfare, transfer, lines]
+    
+    fairfare_circles_list = []
+    transfer_circles_list = []
+    lines_circles_list = []
+    scales = [1, 0.2, 3]
+
+    for station in data["stations"].keys():
+        counts = [data["stations"][station]["times"]["08:00:00.000"]["Metrocard - Fair Fare"]["ridership"], data["stations"][station]["all_times"]["all_fare_classes"]["transfers"], len(data["stations"][station]["lines"])]
+        # print(counts)
+        pos = [map_coords(nyc_svg_bottom_left, nyc_svg_top_right, (0,0), (500,500), (float(data["stations"][station]["latitude"]), float(data["stations"][station]["longitude"]))), map_coords(nyc_svg_bottom_left, nyc_svg_top_right, (0,0), (500,500), (float(data["stations"][station]["latitude"]), float(data["stations"][station]["longitude"]))), map_coords(nyc_svg_bottom_left, nyc_svg_top_right, (0,0), (500,500), (float(data["stations"][station]["latitude"]), float(data["stations"][station]["longitude"])))]
+        #print(pos[0])
+        # print(counts[0]*scales[0]==counts[2]*scales[2])
+        pos[0].append(math.sqrt(counts[0])*scales[0])
+        pos[1].append(math.sqrt(counts[1])*scales[1])
+        pos[2].append(math.sqrt(counts[2])*scales[2])
+        # print(f"adding: {math.sqrt(counts[0])*scales[0]}")
+        # print(pos[0])
+        fairfare_circles_list.append(pos[0])
+        transfer_circles_list.append(pos[1])
+        lines_circles_list.append(pos[2])
+
+    #print("check: "+ str(fairfare_circles_list == lines_circles_list))
+    # print(fairfare_circles_list)
+    #print(transfer_circles_list)
+    # print(lines_circles_list)
     # Definition of the routes. Put them into their own file. See also
     # Flask Blueprints: http://flask.pocoo.org/docs/latest/blueprints
     @app.route("/")
     def home():
-        return  flask.render_template("index.html", boroughs = boroughs, nyc_svg_bottom_left = nyc_svg_bottom_left, nyc_svg_top_right = nyc_svg_top_right, data = data, circles_list = circles_list)
+        return  flask.render_template("index.html", boroughs = boroughs, nyc_svg_bottom_left = nyc_svg_bottom_left, nyc_svg_top_right = nyc_svg_top_right, data = data, fairfare_circles_list = fairfare_circles_list, transfer_circles_list = transfer_circles_list, lines_circles_list = lines_circles_list)
     
-    @app.route("/index")
-    def index():
-        return  flask.render_template("index.html", boroughs = boroughs, nyc_svg_bottom_left = nyc_svg_bottom_left, nyc_svg_top_right = nyc_svg_top_right, data = data, circles_list = circles_list)
+    # @app.route("/index")
+    # def index():
+    #     return  flask.render_template("index.html", boroughs = boroughs, nyc_svg_bottom_left = nyc_svg_bottom_left, nyc_svg_top_right = nyc_svg_top_right, data = data, circles_list = circles_list)
 
     @app.route("/about")
     def about():
@@ -74,7 +103,17 @@ def create_app(config=None):
     @app.route("/borough_data")
     def subway_data():
         borough = flask.request.args.get('borough')
-        return flask.render_template('borough_data.html', borough = borough, boroughs = boroughs, data = data, borough_data = borough_data)
+
+        bor_adds_counter = 0
+        bor_times_sum = 0
+        for station in data["stations"]:
+            if data["stations"][station]["borough"] == borough:
+                for time in data["stations"][station]["times"]:
+                    bor_times_sum += (data["stations"][station]["times"][time]["all_fare_classes"]["ridership"] * int(time[:2]))
+                    bor_adds_counter += data["stations"][station]["times"][time]["all_fare_classes"]["ridership"]
+        bor_avg_time = bor_times_sum/bor_adds_counter
+
+        return flask.render_template('borough_data.html', borough = borough, boroughs = boroughs, data = data, transit_bor = (bor_avg_time < avg_time), very_transit_bor = ((avg_time - bor_avg_time) > 1.5), time_vals = [(i, f"{i:02}:00:00.000") for i in range(24)])
     return app
 
 
--- static/preprocessing/format_data.py
+++ static/preprocessing/format_data.py
@@ -1,29 +1,106 @@
 import pandas as pd
 import json
+import re
 
 df = pd.read_csv("data/data.csv")
-
+fare_types = ["Metrocard - Fair Fare", "Metrocard - Full Fare","Metrocard - Other", "Metrocard - Seniors & Disability","Metrocard - Students", "Metrocard - Unlimited 30-Day", "Metrocard - Unlimited 7-Day", "OMNY - Full Fare", "OMNY - Other", "OMNY - Seniors & Disability"]
 ddict = {}
+
+boroughs = set(df['borough'].tolist())
+
+ddict["boroughs"] = {}
+ddict["stations"] = {}
+ddict["other_data"] = {}
+ddict["other_data"]["boroughs"] = list(boroughs)
+for borough in boroughs:
+    ddict["boroughs"][borough] = {}
+    ddict["boroughs"][borough]["times"] = {}
+    ddict["boroughs"][borough]["all_times"] = {}
+    for fare_type in fare_types:
+            ddict["boroughs"][borough]["all_times"][fare_type] =  {}
+            ddict["boroughs"][borough]["all_times"][fare_type]["ridership"] = 0
+            ddict["boroughs"][borough]["all_times"][fare_type]["transfers"] = 0
+    ddict["boroughs"][borough]["all_times"]["all_fare_classes"] = {}
+    ddict["boroughs"][borough]["all_times"]["all_fare_classes"]["ridership"] = 0
+    ddict["boroughs"][borough]["all_times"]["all_fare_classes"]["transfers"] = 0
 
 for ind in df.index:
     name = df['station_complex'][ind]
     time = df['transit_timestamp'][ind][11:]
+    fare_class = df['fare_class_category'][ind]
+    borough = df["borough"][ind]
 
-    if name not in ddict.keys():
-        ddict[name] = {}
-        ddict[name]["times"] = {}
-    if time not in ddict[name].keys():
-        ddict[name]["times"][time] = {}
+    lines = set()
+    for i in re.findall(r'\(.*?\)', name):
+        i = i[1:-1].split(",")
+        for elem in i:
+            if len(elem) == 1:
+                lines.update(elem)
+    
+    if name not in ddict["stations"].keys():
+        ddict["stations"][name] = {}
+        ddict["stations"][name]["lines"] = list(lines)
+        ddict["stations"][name]["times"] = {}
+        ddict["stations"][name]["all_times"] = {}
+        for fare_type in fare_types:
+            ddict["stations"][name]["all_times"][fare_type] =  {}
+            ddict["stations"][name]["all_times"][fare_type]["ridership"] = 0
+            ddict["stations"][name]["all_times"][fare_type]["transfers"] = 0
+        ddict["stations"][name]["all_times"]["all_fare_classes"] = {}
+        ddict["stations"][name]["all_times"]["all_fare_classes"]["ridership"] = 0
+        ddict["stations"][name]["all_times"]["all_fare_classes"]["transfers"] = 0
+    if time not in ddict["stations"][name]["times"].keys():
+        ddict["stations"][name]["times"][time] = {}
+        for fare_type in fare_types:
+            ddict["stations"][name]["times"][time][fare_type] = {}
+            ddict["stations"][name]["times"][time][fare_type]["ridership"] = 0
+            ddict["stations"][name]["times"][time][fare_type]["transfers"] = 0
+        ddict["stations"][name]["times"][time]["all_fare_classes"] = {}
+        ddict["stations"][name]["times"][time]["all_fare_classes"]["ridership"] = 0
+        ddict["stations"][name]["times"][time]["all_fare_classes"]["transfers"] = 0
+    if time not in ddict["boroughs"][borough]["times"].keys():
+        ddict["boroughs"][borough]["times"][time] = {}
+        for fare_type in fare_types:
+            ddict["boroughs"][borough]["times"][time][fare_type] = {}
+            ddict["boroughs"][borough]["times"][time][fare_type]["ridership"] = 0
+            ddict["boroughs"][borough]["times"][time][fare_type]["transfers"] = 0
+        ddict["boroughs"][borough]["times"][time]["all_fare_classes"] = {}
+        ddict["boroughs"][borough]["times"][time]["all_fare_classes"]["ridership"] = 0
+        ddict["boroughs"][borough]["times"][time]["all_fare_classes"]["transfers"] = 0
     
     station_keys = ["station_complex_id", "borough", "latitude", "longitude", "georeference"]
     for i in station_keys:
-        ddict[name][i] = str(df[i][ind])
+        ddict["stations"][name][i] = str(df[i][ind])
 
-    ride_keys = ["transit_mode","payment_method","fare_class_category","ridership","transfers"]
+    ride_stats = ["ridership","transfers"]
 
-    for i in ride_keys:
-        ddict[name]["times"][time][i] = str(df[i][ind])
+    for i in ride_stats:
+        ddict["stations"][name]["all_times"][fare_class][i] += float(df[i][ind])
+        ddict["stations"][name]["all_times"]["all_fare_classes"][i] += float(df[i][ind])
+        ddict["stations"][name]["times"][time][fare_class][i] = float(df[i][ind])
+        ddict["stations"][name]["times"][time]["all_fare_classes"][i] += float(df[i][ind])
 
-print(ddict)
+        ddict["boroughs"][borough]["all_times"][fare_class][i] += float(df[i][ind])
+        ddict["boroughs"][borough]["all_times"]["all_fare_classes"][i] += float(df[i][ind])
+
+        if fare_class not in ddict["boroughs"][borough]["times"][time]:
+            ddict["boroughs"][borough]["times"][time][fare_class] = {}
+
+        if i not in ddict["boroughs"][borough]["times"][time][fare_class]:
+            ddict["boroughs"][borough]["times"][time][fare_class][i] = 0
+        
+        ddict["boroughs"][borough]["times"][time][fare_class][i] += float(df[i][ind])
+        
+        ddict["boroughs"][borough]["times"][time]["all_fare_classes"][i] += float(df[i][ind])
+
+#print(ddict)
+
+ddict["other_data"]["num_stations"] = len(ddict["stations"].keys())
+
+total_sum = 0
+
+for borough in ddict["boroughs"]:
+    total_sum+=ddict["boroughs"][borough]["all_times"]["all_fare_classes"]["ridership"]
+ddict["other_data"]["average_hourly_ridership_borough"] = total_sum/(len(ddict["boroughs"].keys()) * 24)
 
 json.dump(ddict, open("data/data.json", "w"))
